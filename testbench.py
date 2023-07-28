e2458a330615abffc13131e23a0c2f55013d4eea
Alexander Hering <dev.hering@gmail.com>
2023-07-28 17:39:31 +0200

    adjusted live testing to new structure

--- testbench.py
+++ testbench.py
@@ -11,10 +11,7 @@ from src.utility.bronze import json_utility
 from src.model.scraping_control.archiving.requests_website_archiver import RequestsWebsiteArchiver
 
 if __name__ == "__main__":
-
     PROFILE_PATH = f"{cfg.PATHS.DATA_PATH}/processes/profiles/se80_co_uk.json"
     archiver = RequestsWebsiteArchiver(
         json_utility.load(PROFILE_PATH))
-    # archiver.load_state_dump(os.path.join(
-    #    archiver.profile["offline_copy_path"], "milestone.json"))
     archiver.archive_website()
