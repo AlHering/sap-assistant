f3e132dd7425ba109717f96ea7f569de45b8c0fc
Alexander Hering <dev.hering@gmail.com>
2023-07-29 14:59:15 +0200

    replace non functioning logging temporarily

--- src/configuration/configuration.py
+++ src/configuration/configuration.py
@@ -5,11 +5,11 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+from . import urls as URLS
+from . import paths as PATHS
+from dotenv import dotenv_values
 import os
 import logging
-from dotenv import dotenv_values
-from . import paths as PATHS
-from . import urls as URLS
 
 
 """
@@ -20,5 +20,35 @@ ENV = dotenv_values(os.path.join(PATHS.PACKAGE_PATH, ".env"))
 """
 Logger
 """
-LOGGER = logging.Logger("SAPAssistant")
-LOGGER.setLevel(logging.DEBUG)
+
+
+class LOGGER_REPLACEMENT(object):
+    """
+    Temporary logger replacement class.
+    """
+
+    def debug(self, text: str) -> None:
+        """
+        Method replacement for logging.
+        :param text: Text to log.
+        """
+        print(f"[DEBUG] {text}")
+
+    def info(self, text: str) -> None:
+        """
+        Method replacement for logging.
+        :param text: Text to log.
+        """
+        print(f"[INFO] {text}")
+
+    def warning(self, text: str) -> None:
+        """
+        Method replacement for logging.
+        :param text: Text to log.
+        """
+        print(f"[WARNING] {text}")
+
+
+# LOGGER = logging.Logger("SAPAssistant")
+# LOGGER.setLevel(level=logging.INFO)
+LOGGER = LOGGER_REPLACEMENT()
