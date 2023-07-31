4e0c32d751e6c45dac6f1c5149321e3fc20627c2
Alexander Hering <dev.hering@gmail.com>
2023-07-31 23:46:08 +0200

    reorganized multiprocessing

--- testbench2.py
+++ testbench2.py
@@ -5,34 +5,35 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
-from src.model.scraping_control.archiving.requests_website_archiver import RequestsWebsiteArchiver
-import os
+from time import sleep
 from multiprocessing import Process
 from src.configuration import configuration as cfg
+from src.model.scraping_control.archiving.requests_website_archiver import RequestsWebsiteArchiver
 from src.utility.bronze import json_utility
-from src.model.scraping_control.archiving.parameterized_website_database import WebsiteDatabase
-from src.model.scraping_control.archiving import website_archiver
-website_archiver.website_database
 
 
-def run_archiver(profile: dict) -> None:
+def run_archiver(profile: dict, wait: float = .0) -> None:
     """
     Function for running profile-based archiver.
     :param profile: Archiver profile.
+    :param wait: Time to wait in seconds before starting.
     """
-
+    sleep(wait)
     archiver = RequestsWebsiteArchiver(profile)
     archiver.archive_website()
 
 
 if __name__ == "__main__":
     processes = []
-    for profile_name in []:
+    counter = .0
+    for profile_name in ["tcodesearch_com", "sapdatasheet_org", "sap4tech_net", "erpgreat_com", "erp-up_de"]:
         profile = json_utility.load(
             f"{cfg.PATHS.DATA_PATH}/processes/profiles/{profile_name}.json")
         database_uri = f"sqlite:///{cfg.PATHS.DATA_PATH}/processes/{profile_name}.db"
         profile["database_uri"] = database_uri
-        processes.append(Process(target=run_archiver, args=(profile,)))
+        processes.append(Process(target=run_archiver,
+                         args=(profile, counter*10)))
         processes[-1].start()
+        counter += 1
     for process in processes:
         process.join()
