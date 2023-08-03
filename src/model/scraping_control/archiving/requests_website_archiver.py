466c5b1c095c8eaa4594279ec92468721795c56e
Alexander Hering <dev.hering@gmail.com>
2023-08-03 19:05:08 +0200

    extracted exception heavy step and handling fails

--- src/model/scraping_control/archiving/requests_website_archiver.py
+++ src/model/scraping_control/archiving/requests_website_archiver.py
@@ -82,6 +82,7 @@ class RequestsWebsiteArchiver(WebsiteArchiver):
         json_utility.save(
             {
                 "_cache": {key: self._cache[key] for key in self._cache if key != "session"},
+                "failed": list(self.failed),
                 "reason": reason
             },
             os.path.join(
@@ -97,6 +98,7 @@ class RequestsWebsiteArchiver(WebsiteArchiver):
         """
         dump_data = json_utility.load(path)
         self._cache.update(dump_data["_cache"])
+        self.failed = set(dump_data.get("failed", []))
 
     def _retry_with_new_identity(self) -> requests.Response:
         """
@@ -121,12 +123,11 @@ class RequestsWebsiteArchiver(WebsiteArchiver):
 
         return response if (response is not None and response.status_code != 200) else self._cache["session"].get(self._cache["current_url"], headers={"User-agent": internet_utility.get_user_agent()})
 
-    def _handle_next_page(self, next_url: str) -> None:
+    def _request_page(self) -> Optional[requests.Response]:
         """
-        Internal method to handle next page.
-        :param next_url: Next page URL.
+        Internal method for requesting page and handling errors.
         """
-        self._cache["current_url"] = next_url
+        response = None
         try:
             self.logger.info(
                 f"[{self.profile['base_url']}] Fetching {self._cache['current_url']} ({self._cache['current_index']})")
@@ -134,6 +135,8 @@ class RequestsWebsiteArchiver(WebsiteArchiver):
             if response.status_code != 200:
                 response = self._retry_with_new_identity()
         except MaxRetryError:
+            self.logger.warning(
+                f"[{self.profile['base_url']}] Max retry error appeared! Trying with different identity...")
             response = self._retry_with_new_identity()
         except SSLError:
             self.logger.warning(
@@ -146,7 +149,7 @@ class RequestsWebsiteArchiver(WebsiteArchiver):
             self._cache["current_url"] = self.fix_link(
                 self._cache["last_url"], self._cache["current_url"])
             response = self._cache["session"].get(self._cache["current_url"])
-        except requests.exceptions.ConnectionError as ex:
+        except requests .exceptions.ConnectionError as ex:
             self.create_state_dump({
                 "exception": str(ex),
                 "traceback": traceback.format_exc()
@@ -154,57 +157,73 @@ class RequestsWebsiteArchiver(WebsiteArchiver):
             # TODO: Implement appropriate methods on super-class level
             self.logger.warning(
                 f"[{self.profile['base_url']}] Connection error appeared! State dump created.")
-            tries = 0
-            while not internet_utility.check_connection() and self._cache["reconnect_retries"] < tries:
-                self.logger.warning(
-                    f"[{self.profile['base_url']}] No internet connection! Retrying in 10 seconds ...")
-                time.sleep(self._cache["reconnect_interval"])
-                tries += 1
-            self.logger.info(
-                f"[{self.profile['base_url']}] Regained internet connection, retrying ...")
-            return self._handle_next_page(self._cache["current_url"])
-        self.logger.info(
-            f"[{self.profile['base_url']}] Status: {response.status_code}")
-        self.register_page(self._cache["current_url"], response.content)
-
-        html_content = html.fromstring(
-            response.content if response.content else "<!DOCTYPE html><html>")
-
-        target_pages = list(
-            set([self.fix_link(response.url, elem) for elem in html_content.xpath("//@href | //@src | //@data-src")]))
-        target_assets = []
-        for page_link in [link for link in target_pages if
-                          "." in urlparse(link).path.split("/")[-1] and ".html" not in urlparse(link).path.split("/")[
-                              -1].lower() and ".php" not in urlparse(link).path.split("/")[
-                              -1].lower()]:
-            if page_link not in target_assets:
-                target_assets.append(page_link)
-            target_pages.remove(page_link)
-
-        for link in target_assets:
-            self.register_link(self._cache["current_url"], link, "asset")
-            try:
-                asset_data = self.get_asset_data(link)
-                self.register_asset(
-                    self._cache["current_url"], link, *asset_data)
-            except requests.exceptions.MissingSchema:
+            if not internet_utility.check_connection():
+                tries = 0
+                while not internet_utility.check_connection() and self._cache["reconnect_retries"] < tries:
+                    self.logger.warning(
+                        f"[{self.profile['base_url']}] No internet connection! Retrying in 10 seconds ...")
+                    time.sleep(self._cache["reconnect_interval"])
+                    tries += 1
                 self.logger.info(
-                    f"[{self.profile['base_url']}] Schema exception appeared for '{link}'")
-            except requests.exceptions.ConnectionError:
-                self.logger.info(
-                    f"[{self.profile['base_url']}] ConnectionError exception appeared for '{link}'")
-
-        discarded = 0
-        discarded_external = 0
-        for link in target_pages:
-            link_netloc = urlparse(link).netloc
-            if any(base in link_netloc for base in self.allowed_bases):
-                newly_created = self.register_link(
-                    self._cache["current_url"], link, "page")
-                if not newly_created:
-                    discarded += 1
+                    f"[{self.profile['base_url']}] Regained internet connection, retrying ...")
+                return self._handle_next_page(self._cache["current_url"])
             else:
-                discarded_external += 1
-        self.logger.info(
-            f"[{self.profile['base_url']}] Discarded {discarded} internal and {discarded_external} external page links.")
-        self._cache["current_index"] += 1
+                self.failed.add(self._cache["current_url"])
+                self.logger.info(
+                    f"[{self.profile['base_url']}] '{self._cache['current_url']}' not reachable, ignoring ...")
+        return response
+
+    def _handle_next_page(self, next_url: str) -> None:
+        """
+        Internal method to handle next page.
+        :param next_url: Next page URL.
+        """
+        self._cache["current_url"] = next_url
+        response = self._request_page()
+        if response is not None and response.status_code == 200:
+            # Processing response
+            self.logger.info(
+                f"[{self.profile['base_url']}] Status: {response.status_code}")
+            self.register_page(self._cache["current_url"], response.content)
+
+            html_content = html.fromstring(
+                response.content if response.content else "<!DOCTYPE html><html>")
+
+            target_pages = list(
+                set([self.fix_link(response.url, elem) for elem in html_content.xpath("//@href | //@src | //@data-src")]))
+            target_assets = []
+            for page_link in [link for link in target_pages if
+                              "." in urlparse(link).path.split("/")[-1] and ".html" not in urlparse(link).path.split("/")[
+                                  -1].lower() and ".php" not in urlparse(link).path.split("/")[
+                                  -1].lower()]:
+                if page_link not in target_assets:
+                    target_assets.append(page_link)
+                target_pages.remove(page_link)
+
+            for link in target_assets:
+                self.register_link(self._cache["current_url"], link, "asset")
+                try:
+                    asset_data = self.get_asset_data(link)
+                    self.register_asset(
+                        self._cache["current_url"], link, *asset_data)
+                except requests.exceptions.MissingSchema:
+                    self.logger.info(
+                        f"[{self.profile['base_url']}] Schema exception appeared for '{link}'")
+                except requests.exceptions.ConnectionError:
+                    self.logger.info(
+                        f"[{self.profile['base_url']}] ConnectionError exception appeared for '{link}'")
+
+            discarded = 0
+            discarded_external = 0
+            for link in target_pages:
+                link_netloc = urlparse(link).netloc
+                if any(base in link_netloc for base in self.allowed_bases):
+                    newly_created = self.register_link(
+                        self._cache["current_url"], link, "page")
+                    if not newly_created:
+                        discarded += 1
+                else:
+                    discarded_external += 1
+            self.logger.info(
+                f"[{self.profile['base_url']}] Discarded {discarded} internal and {discarded_external} external page links.")
+            self._cache["current_index"] += 1
