359d475868beb0c2a9302ce5c0250a0f9d52d696
Alexander Hering <dev.hering@gmail.com>
2023-08-05 17:49:54 +0200

    adjusted import to structural changes

--- testbench.py
+++ testbench.py
@@ -8,7 +8,7 @@
 import os
 from src.configuration import configuration as cfg
 from src.utility.bronze import json_utility
-from src.model.scraping_control.archiving.requests_website_archiver import RequestsWebsiteArchiver
+from src.model.scraping_control.archiving.requests_website_database_archiver import RequestsWebsiteArchiver
 
 if __name__ == "__main__":
     profile = json_utility.load(
