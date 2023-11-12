0737b70d91441edc89fb97e903e6ebfb9ef3b3f7
Alexander Hering <dev.hering@gmail.com>
2023-11-12 22:29:04 +0100

    implemented parallel migration

--- testbench4.py
+++ testbench4.py
@@ -10,6 +10,7 @@ from multiprocessing import Process
 from src.configuration import configuration as cfg
 from src.model.scraping_control.archiving.requests_website_archiver import RequestsWebsiteArchiver
 from src.utility.bronze import json_utility, sqlalchemy_utility
+from src.utility.silver import file_system_utility
 
 
 def print_fk_info(tables):
@@ -28,18 +29,32 @@ def run_migration(su, tu, st, tt):
     sqlalchemy_utility.migrate(su, tu, st, tt)
 
 
-if __name__ == "__main__":
-    profile_name = "se80_co_uk"
+def run_parallel_migration(profiles):
+    processes = []
+    for profile_name in profiles:
+        profile = json_utility.load(
+            f"{cfg.PATHS.DATA_PATH}/processes/profiles/{profile_name}.json")
+        su = f"sqlite:///{cfg.PATHS.DATA_PATH}/processes/backups/{profile_name}.db"
+        tu = f"sqlite:///{cfg.PATHS.DATA_PATH}/processes/{profile_name}.db"
+        profile["database_uri"] = su
+        archiver = RequestsWebsiteArchiver(profile)
+        db = archiver.database
+        tables = db.base.metadata.tables
+        st = [t for t in tables if t.startswith("1.")]
+        tt = [t for t in tables if t.startswith(
+            file_system_utility.clean_directory_name(profile["base_url"]))]
+        processes.append(Process(target=run_migration,
+                         args=(su, tu, st, tt,)))
+        processes[-1].start()
+    try:
+        for process in processes:
+            process.join()
+    except KeyboardInterrupt:
+        for process in processes:
+            process.kill()
 
-    profile = json_utility.load(
-        f"{cfg.PATHS.DATA_PATH}/processes/profiles/{profile_name}.json")
-    database_uri = f"sqlite:///{cfg.PATHS.DATA_PATH}/processes/{profile_name}.db"
-    profile["database_uri"] = f"sqlite:///{cfg.PATHS.DATA_PATH}/processes/backups/{profile_name}.db"
-    archiver = RequestsWebsiteArchiver(profile)
-    db = archiver.database
-    source_classes = sqlalchemy_utility.get_classes_from_base(db.base)
-    tables = db.base.metadata.tables
-    st = [t for t in tables if t.startswith("1.")]
-    tt = [t for t in tables if t.startswith("https-__www-se80-co-uk_.")]
-    run_migration(
-        f"sqlite:///{cfg.PATHS.DATA_PATH}/processes/backups/{profile_name}.db", database_uri, st, tt)
+
+if __name__ == "__main__":
+    profiles = ["tcodesearch_com", "sapdatasheet_org",
+                "sap4tech_net", "erpgreat_com", "erp-up_de"]
+    run_parallel_migration(profiles)
