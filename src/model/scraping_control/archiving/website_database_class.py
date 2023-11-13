f76d65f04d5fa37ec74df8e0a339e67daf62e17e
Alexander Hering <dev.hering@gmail.com>
2023-11-13 12:48:41 +0100

    giving back empty dict if no cache not existing

--- src/model/scraping_control/archiving/website_database_class.py
+++ src/model/scraping_control/archiving/website_database_class.py
@@ -74,7 +74,7 @@ class WebsiteDatabase(BasicSQLAlchemyInterface):
         """
         Method for getting the cache.
         """
-        return copy.deepcopy(self.run.cache)
+        return {} if self.run.cache is None else copy.deepcopy(self.run.cache)
 
     def update_cache(self, cache: dict) -> None:
         """
